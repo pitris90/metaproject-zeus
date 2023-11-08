295b0a1de9a830e3231972149e75151a86dc0f70
Adam Valalský <492900@mail.muni.cz>
2023-11-08 12:21:07 +0100

    feat: load and validate environment variables

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from '../config/validationSchema.config';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			validationSchema,
+			isGlobal: true
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
