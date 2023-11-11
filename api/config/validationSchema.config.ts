4740956dae639a986e3477ebb1dd8b435a341d4f
Adam Valalský <492900@mail.muni.cz>
2023-11-11 16:00:48 +0100

    feat: added postgres to docker

--- api/config/validationSchema.config.ts
+++ api/config/validationSchema.config.ts
@@ -1,5 +1,11 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-	APPLICATION_MODE: Joi.string().valid('development', 'production', 'test')
+	APPLICATION_MODE: Joi.string().valid('development', 'production', 'test'),
+	// database
+	POSTGRES_HOST: Joi.string(),
+	POSTGRES_PORT: Joi.number(),
+	POSTGRES_USER: Joi.string(),
+	POSTGRES_PASSWORD: Joi.string(),
+	POSTGRES_DATABASE: Joi.string()
 });
