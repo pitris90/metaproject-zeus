082c5c51b0e59192c60568fffe2e9dac33c71ea7
Adam Valalský <492900@mail.muni.cz>
2024-02-25 13:59:45 +0100

    feat(api): prepare decorator for public endpoints

--- api/src/auth-module/guards/auth.guard.ts
+++ api/src/auth-module/guards/auth.guard.ts
@@ -1,8 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+	constructor(private readonly reflector: Reflector) {}
+
 	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+			context.getHandler(),
+			context.getClass()
+		]);
+
+		if (isPublic) {
+			return true;
+		}
+
 		const request = context.switchToHttp().getRequest();
 
 		// TODO this will be probably some token or something, for now we just send ID directly
