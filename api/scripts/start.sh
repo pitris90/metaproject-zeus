911f3c8a4461e20eaa125d3dcd4977f89565505a
Petr Balnar <petr.balnar@seznam.cz>
2025-11-11 15:38:12 +0100

    set git user and email in container

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -6,4 +6,12 @@ if [ -n "$OPENSTACK_GITLAB_TOKEN" ]; then
     "!f() { echo username=oauth2; echo password=$OPENSTACK_GITLAB_TOKEN; }; f"
 fi
 
+if [ -n "$OPENSTACK_GIT_AUTHOR_NAME" ]; then
+  git config --global user.name "$OPENSTACK_GIT_AUTHOR_NAME"
+fi
+
+if [ -n "$OPENSTACK_GIT_AUTHOR_EMAIL" ]; then
+  git config --global user.email "$OPENSTACK_GIT_AUTHOR_EMAIL"
+fi
+
 exec npm run start:dev
